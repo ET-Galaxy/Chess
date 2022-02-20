90fb2c28a495ee9fd2c95323894d5309e1a89d20
ET_Galaxy <edwintang314@gmail.com>
2022-02-20 12:50:19 +0000

    King and Queen

--- initialpositions.py
+++ initialpositions.py
@@ -1,5 +1,13 @@
-import Pawn
-APawnWhite=Pawn('a2', True, 'white', True)
-BPawnWhite=Pawn('b2', True, 'white', True)
+from chesspieces import Chess_Pieces
+from chesspieces import Queen
+from chesspieces import King
+
+#AWPawn=Pawn([1,2], True, 'white', True)
+#BWPawn=Pawn([2,2], True, 'white', True)
+#ABPawn=Pawn([1,7], True, 'black', True)
+#BBPawn=Pawn([2,7], True, 'black', True)
+WKing=King([5,1], False, 'white')
+BKing=King([5,8], False, 'black')
+WQueen=Queen([4,1], True, 'white')
 
 print(BPawnWhite.alive)
